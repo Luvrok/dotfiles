d4a1263cb6aefd855eceedf19db0013d4280318b
Luvrok <morturion.life@gmail.com>
2026-01-23 00:23:04 +0300

    refactor(yazi): rework theme, plugins and keybindings

--- home/programs/yazi/default.nix
+++ home/programs/yazi/default.nix
@@ -1,36 +1,58 @@
 { pkgs, config, lib, ... }:
+
+# references
+# docs -> yazi-rs.github.io/docs/
+# 1(nix) -> https://github.com/sk1lax/bspwm-dots/blob/42a2271cf99195d30f20c7144dad75bee1f1adc6/config/yazi/yazi.toml
+# yatline-githead -> https://github.com/imsi32/yatline-githead.yazi
+
 let
   inherit (lib) getExe;
 in
 {
   programs.yazi = {
     enable = true;
-    package = pkgs.yazi;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
 
     plugins = {
       inherit (pkgs.yaziPlugins) full-border;
       inherit (pkgs.yaziPlugins) smart-enter;
+      # inherit (pkgs.yaziPlugins) yatline-githead; # wait when this pr will be merge https://github.com/imsi32/yatline-githead.yazi/pull/7
+      # inherit (pkgs.yaziPlugins) git; # dont work, maybe bcs nixos i think (todo: figure it out)
       xclip-system-clipboard = pkgs.fetchFromGitHub {
         owner = "seqizz";
         repo = "xclip-system-clipboard.yazi";
         rev = "67f63710c892e9443c81df8dd396b93ff658a9ba";
         hash = "sha256-+jzVFosUjNHKAdOrRz4HXNweFggUoWoGMtYv2L3GYyA=";
       };
-    };
-
-    initLua = ''
-      require("full-border"):setup()
-    '';
-
-    theme = {
-      icon = {
-        globs = [];
-        dirs  = [];
-        files = [];
-        exts  = [];
-        conds = [];
+      yatline = pkgs.fetchFromGitHub {
+        owner = "imsi32";
+        repo = "yatline.yazi";
+        rev = "3227a30b21f69b68df513754b5a00d6e75cece57";
+        hash = "sha256-yhptHABQ0alVab2i367D5grJyG7SrfHH8H4JuGeYFyk=";
+      };
+      yatline-gruvbox = pkgs.fetchFromGitHub {
+        owner = "imsi32";
+        repo = "yatline-gruvbox.yazi";
+        rev = "1ce46ebe1f48139de30051638d7c6ba71d867220";
+        hash = "sha256-Dsf6FlUmGQTLSM63++Wa9ChQmTqhAp03rIWYtS1rrv8=";
       };
     };
+
+    initLua = ./init.lua;
+
+    # cant disable icons when yatline is enable looks like this bug(but its not):
+    # https://github.com/imsi32/yatline.yazi/issues/61
+    # theme = {
+    #   prepend_rules = true;
+    #   icon = {
+    #     globs = [];
+    #     dirs  = [];
+    #     files = [];
+    #     exts  = [];
+    #     conds = [];
+    #   };
+    # };
 
     keymap = {
       input = {
@@ -90,9 +112,13 @@ in
             run = "link --relative";
             desc = "link file";
           }
-          # https://yazi-rs.github.io/docs/tips#smart-enter
           {
             on = ["<Enter>"];
+            run = "plugin --sync smart-enter";
+            desc = "Enter directory";
+          }
+          {
+            on = ["<Right>"];
             run = "plugin --sync smart-enter";
             desc = "Enter directory";
           }
@@ -116,8 +142,9 @@ in
           4
         ];
         sort_by = "natural";
-        linemod = "none";
+        # linemode = "size"; # folder count works so bad
         show_hidden = false;
+        show_symlink = true;
       };
       opener = {
         edit = [
---
+++ home/programs/yazi/init.lua
@@ -0,0 +1,61 @@
+local gruvbox_theme = require("yatline-gruvbox"):setup("dark")
+
+require("full-border"):setup {
+  type = ui.Border.PLAIN,
+}
+
+require("yatline"):setup({
+  theme = gruvbox_theme,
+
+  section_separator = { open = "", close = "" },
+  part_separator = { open = "", close = "" },
+  inverse_separator = { open = "tab", close = "" },
+
+  tab_width = 20,
+
+  show_background = false,
+
+  display_header_line = true,
+  display_status_line = true,
+
+  header_line = {
+    left = {
+      section_a = {
+        { type = "string", custom = false, name = "tab_path", params = { trimed = false, max_length=24, trim_length=10 } },
+      },
+      section_b = {},
+      section_c = {},
+    },
+    right = {
+      section_a = {
+        { type = "string", custom = false, name = "date", params = {"%A, %d %B %Y"} },
+      },
+      section_b = {},
+      section_c = {},
+    },
+  },
+
+  status_line = {
+    left = {
+      section_a = {
+        { type = "string", custom = false, name = "tab_mode" },
+      },
+      section_b = {
+        { type = "string", custom = false, name = "hovered_size" },
+      },
+      section_c = {
+        { type = "string", custom = false, name = "hovered_name" },
+      },
+    },
+    right = {
+      section_a = {
+      },
+      section_b = {
+        { type = "string", custom = false, name = "cursor_percentage" },
+      },
+      section_c = {
+        { type = "coloreds", custom = false, name = "permissions" },
+      },
+    },
+  },
+})
