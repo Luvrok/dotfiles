0b13bbc990a63fa276e5498d7c76fc85a9ee8d3b
Luvrok <morturion.life@gmail.com>
2026-04-19 04:37:18 +0300

    refactor(scripts): move to POSIX, minor updates

--- hosts/barnard/env.nix
+++ hosts/barnard/env.nix
@@ -24,11 +24,18 @@
 
   environment.etc."X11/xorg.conf.d/60-monitor.conf".text = ''
     Section "Monitor"
-      Identifier "HDMI-A-1"
-      Modeline "2560x1440R"  497.25  2560 2608 2640 2720  1440 1443 1448 1525 +hsync -vsync
-      Option "PreferredMode" "2560x1440R"
-      Option "Position" "0 0"
-      Option "Primary" "true"
+        Identifier "DisplayPort-0"
+        Modeline "2560x1440R" 497.25 2560 2608 2640 2720 1440 1443 1448 1525 +hsync -vsync
+        Option "PreferredMode" "2560x1440R"
+        Option "Position" "0 0"
+    EndSection
+
+    Section "Monitor"
+        Identifier "DisplayPort-1"
+        Modeline "2560x1440R" 497.25 2560 2608 2640 2720 1440 1443 1448 1525 +hsync -vsync
+        Option "PreferredMode" "2560x1440R"
+        Option "Position" "2560 0"
+        Option "Primary" "true"
     EndSection
   '';
 }
