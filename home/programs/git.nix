9d896c961569b18cc5bb6b64c941487106be2415
Luvrok <morturion.life@gmail.com>
2026-04-24 19:05:49 +0300

    chore(git): update user email

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -5,7 +5,7 @@
     enable = true;
     package = pkgs.git;
     settings = {
-      user.email = "morturion.life@gmail.com";
+      user.email = "pawel.2020.navtop@gmail.com";
       user.name = "Luvrok";
       init.defaultBranch = "master";
     };
