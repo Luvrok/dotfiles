5f3e4d85942eb29d9ae2bbc32fadc740fdacd8c7
Luvrok <pawel.2020.navtop@gmail.com>
2026-05-07 21:44:20 +0300

    feat(neovim): gruvbox: add custom treesitter and lsp highlights

--- home/programs/neovim/nvim/lua/plugins/gruvbox.lua
+++ home/programs/neovim/nvim/lua/plugins/gruvbox.lua
@@ -5,15 +5,70 @@ return {
 	opts = {
 		contrast = "soft",
 		overrides = {
+			-- syntax
 			["@comment"] = { fg = "#928374", italic = false },
-			["@keyword"] = { italic = false },
-			["@parameter"] = { italic = false },
-			["@type"] = { italic = false },
+
+			["@keyword"] = { fg = "#cc241d", italic = false },
+			["@keyword.function"] = { fg = "#cc241d", italic = false },
+			["@keyword.return"] = { fg = "#cc241d", italic = false },
+			["@conditional"] = { fg = "#cc241d", italic = false },
+			["@repeat"] = { fg = "#cc241d", italic = false },
+
+			["@variable"] = { fg = "#ebdbb2" },
+			["@variable.builtin"] = { fg = "#d65d0e" },
+			["@variable.parameter"] = { fg = "#b16286", italic = false },
+			["@parameter"] = { fg = "#b16286", italic = false },
+
+			["@constant"] = { fg = "#b16286" },
+			["@constant.builtin"] = { fg = "#d65d0e" },
+			["@number"] = { fg = "#b16286" },
+			["@boolean"] = { fg = "#b16286" },
+
+			["@string"] = { fg = "#98971a" },
+			["@character"] = { fg = "#98971a" },
+
+			["@function"] = { fg = "#689d6a" },
+			["@function.call"] = { fg = "#689d6a" },
+			["@function.builtin"] = { fg = "#d65d0e" },
+			["@method"] = { fg = "#689d6a" },
+			["@method.call"] = { fg = "#689d6a" },
+			["@constructor"] = { fg = "#d79921" },
+
+			["@type"] = { fg = "#d79921", italic = false },
+			["@type.builtin"] = { fg = "#d79921", italic = false },
+			["@type.definition"] = { fg = "#d79921", italic = false },
+
+			["@property"] = { fg = "#458588" },
+			["@field"] = { fg = "#458588" },
+			["@namespace"] = { fg = "#458588" },
+			["@module"] = { fg = "#458588" },
+
+			["@operator"] = { fg = "#d65d0e" },
+			["@punctuation"] = { fg = "#a89984" },
+			["@punctuation.bracket"] = { fg = "#a89984" },
+			["@punctuation.delimiter"] = { fg = "#a89984" },
+
+			["@tag"] = { fg = "#cc241d" },
+			["@tag.attribute"] = { fg = "#d79921" },
+			["@tag.delimiter"] = { fg = "#a89984" },
+
+			-- lsp semantic tokens
+			["@lsp.type.variable"] = { fg = "#ebdbb2" },
+			["@lsp.type.parameter"] = { fg = "#b16286" },
+			["@lsp.type.property"] = { fg = "#458588" },
+			["@lsp.type.method"] = { fg = "#689d6a" },
+			["@lsp.type.function"] = { fg = "#689d6a" },
+			["@lsp.type.type"] = { fg = "#d79921" },
+			["@lsp.type.struct"] = { fg = "#d79921" },
+			["@lsp.type.interface"] = { fg = "#d79921" },
+			["@lsp.type.enum"] = { fg = "#d79921" },
+			["@lsp.type.keyword"] = { fg = "#cc241d" },
 
 			SignColumn = { bg = "NONE" },
 			TabLineFill = { bg = "#282828" },
 			TabLineSel = { bg = "#282828" },
 			CursorLine = { bg = "#282828" },
+			Visual = { bg = "#3c3836" },
 
 			NeoTreeDirectoryName = { fg = "#a89984" },
 			NeoTreeRootName = { fg = "#d65d0e" },
@@ -21,17 +76,20 @@ return {
 
 			NeoTreeGitAdded = { fg = "#98971a" },
 			NeoTreeGitConflict = { fg = "#b16286" },
-      NeoTreeGitDeleted = { fg = "#cc241d" },
-      NeoTreeGitIgnored = { fg = "#393939" },
-      NeoTreeGitModified = { fg = "#d65d0e" },
-      NeoTreeGitUntracked = { fg = "#d79921" },
+			NeoTreeGitDeleted = { fg = "#cc241d" },
+			NeoTreeGitIgnored = { fg = "#393939" },
+			NeoTreeGitModified = { fg = "#d65d0e" },
+			NeoTreeGitUntracked = { fg = "#d79921" },
+
+			GitSignsAdd = { fg = "#6c7a2a", bg = "NONE" },
+			GitSignsChange = { fg = "#8a5a2a", bg = "NONE" },
+			GitSignsDelete = { fg = "#7a3a38", bg = "NONE" },
+			GitSignsTopdelete = { fg = "#7a3a38", bg = "NONE" },
+			GitSignsChangedelete = { fg = "#8a5a2a", bg = "NONE" },
+			GitSignsUntracked = { fg = "#8a7a2a", bg = "NONE" },
 
 			-- colorscheme
 			-- "#fbf1c7", "#ebdbb2", "#d5c4a1", "#bdae93", "#a89984", "#928374", "#d65d0e", "#cc241d", "#98971a", "#458588", "#b16286", "#689d6a", "#d79921", "#fbf1c7"
-
-			Visual = {
-				bg = "#3c3836",
-			},
 		},
 	},
 	config = function(_, opts)
