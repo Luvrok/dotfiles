76bde8a6d0a08153ceae2125f9ccc6d35d9e0c98
Luvrok <pawel.2020.navtop@gmail.com>
2026-05-31 16:21:20 +0300

    chore(ollama): update model list

--- nixos/services/ollama.nix
+++ nixos/services/ollama.nix
@@ -8,11 +8,12 @@
   environmentVariables = {
     OLLAMA_FLASH_ATTENTION = "1";
     OLLAMA_MAX_LOADED_MODELS = "1";
-    OLLAMA_MAX_VRAM = "0.9";
   };
   loadModels = [
     "gpt-oss:latest"
-    "gemma4:e4b"
     "qooba/qwen3-coder-30b-a3b-instruct:q3_k_m"
+    "codegemma:latest"
+    "qwen2.5-coder:14b"
+    "qwen2.5:14b"
   ];
 }
