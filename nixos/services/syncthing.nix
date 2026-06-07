a4e70419b21026b6e45d81f3a9e5e8d5e5e3c55f
Luvrok <pawel.2020.navtop@gmail.com>
2026-06-08 00:50:57 +0300

    feat(syncthing): add proxy for syncthing bcs putin block relays

--- nixos/services/syncthing.nix
+++ nixos/services/syncthing.nix
@@ -4,6 +4,7 @@
   enable = true;
   user = "${username}";
   openDefaultPorts = true;
+  all_proxy = "socks5://localhost:10808";
 
   dataDir = "/home/${username}/.config/syncthing";
   configDir = "/home/${username}/.config/syncthing";
