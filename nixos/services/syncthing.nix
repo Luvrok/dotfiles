efd15c4952098318baed1f28f528534ed2013088
Luvrok <pawel.2020.navtop@gmail.com>
2026-07-01 00:56:32 +0300

    refactor(barnard-syncthing): remove proxy, it works again without it

--- nixos/services/syncthing.nix
+++ nixos/services/syncthing.nix
@@ -4,7 +4,6 @@
   enable = true;
   user = "${username}";
   openDefaultPorts = true;
-  all_proxy = "socks5://localhost:10808";
 
   dataDir = "/home/${username}/.config/syncthing";
   configDir = "/home/${username}/.config/syncthing";
